6035d0776e254887db4d69763a46452beafd84e8
Traiko <98191053+srTraiko@users.noreply.github.com>
2022-12-09 08:20:39 -0400

    uwu

--- TD.sh
+++ TD.sh
@@ -2,70 +2,96 @@
 # Backups de archivos de configuracion para mi sistema Linux basado en Arch
 # Autor: Traiko - @SrTraiko
 
-# paths
+# Dotfiles locals
 alacritty="$HOME/.config/alacritty/"
 i3="$HOME/.config/i3/"
 kitty="$HOME/.config/kitty/"
 nvim="$HOME/.config/nvim/"
 
-mi_ruta=$(pwd)
-config=$mi_ruta/.config/
+# Backup
+my_route=$(pwd)
+my_route_config="$my_route/.config_no_exist/"
 
-echo -e "Backups de archivos.dotfiles"
-echo "
-1. Clonar directorios y archivos
-2. Salir
-"
-read -p "Ingresa una opcion: " opcion
+option=0
 
-# Verificar si existe el directorio .config
-if [[ -d $config ]]; then
+select_option () {
+  while [ "$option" != 1 -a "$option" != 2 ]
+  do
+    clear
+    echo -e "
+-------------------------------------
+| Backup by Traiko                  |
+-------------------------------------
+| 1. Backups directories and files. |
+| 2. Exit.                          |
+-------------------------------------\n"
+    read -n 1 -s -p "Enter an option: " option
+  done
   echo ""
-else
-  echo "Creando directorio .config"
-  mkdir $config
-fi
+}
 
-if [[ $opcion == "1" ]]; then
-  # alacritty
-  if [[ -d $alacritty ]]; then
-    echo "Clonando alacritty"
-    rm -rf $config/alacritty/
-    cp -r $HOME/.config/alacritty/ $config
+verify_directories () {
+  if [ -d ${1} ]
+  then
+    echo "route '${1}' existence "
   else
-    echo -e "No se encuetra un directorio en la ruta $alacritty\n por lo que no se copiara nada"
+    echo -e "route '${1}' no existence,\nwant to create it? [y/n]"
+    option=""
+
+    while [ "$option" != "y" -a "$option" != "n" ]
+    do 
+      read -n 1 -s option
+
+      case $option in
+        "y")
+          mkdir ${1}
+          ;;
+        "n")
+          echo -e "Danger: the '${1}' directory will not be created and this may cause several problems when creating a backup."
+          ;;
+        esac
+    done
   fi
+}
 
-  # kitty
-  if [[ -d $kitty ]]; then
-    echo "Clonando kitty"
-    rm -rf $config/kitty/
-    cp -r $HOME/.config/kitty/ $config
+cp_files () {
+  if [ -d "${1}" ]
+  then
+    echo "file '${1}' existence"
+    echo "you want to make a backup of '${1}'? [y/n]"
+
+    option=""
+    while [ "$option" != "y" -a "$option" != "n" ]
+    do
+      read -n 1 -s option
+      case $option in
+        "y")
+          echo "creating a backup copy of ${1}"
+          cp -R ${1} ${2}
+          ;;
+        "n")
+          echo "okay"
+          echo "Okay, will not create a backup copy of ${1}"
+          ;; 
+      esac
+    done
   else
-    echo -e "No se encuetra un directorio en la ruta $kitty\n por lo que no se copiara nada"
+    echo "The file '${1} no existence"
   fi
+}
 
-  # i3
-  if [[ -d $i3 ]]; then
-    echo "Clonando i3"
-    rm -rf $config/i3/
-    cp -r $HOME/.config/i3/ $config
-  else
-    echo -e "No se encuetra un directorio en la ruta $i3\n por lo que no se copiara nada"
-  fi
 
-  # nvim
-  if [[ -d $nvim ]]; then
-    echo "Clonando nvim"
-    rm -rf $config/nvim/
-    cp -r $HOME/.config/nvim/ $config
-  else
-    echo -e "No se encuetra un directorio en la ruta $i3\n por lo que no se copiara nada"
-  fi 
 
-elif [[ $opcion == "2" ]]; then
-  echo "Okey..."
-else
-  echo "Esa opcion no es valida, saliendo del programa."
-fi
 
+select_option
+
+case $option in
+  1)
+    echo "verifying the existence of directories"
+    verify_directories $my_route_config 
+    cp_files $alacritty $my_route_config
+    ;;
+  2)
+    echo "Goodbye..."
+    ;;
+esac
